309a2ca5f69a6f78fcf57d00f0561f36597f8c78
SantiZ2610 <santiagoemanuel_2000@live.com.ar>
2025-03-25 13:45:20 -0300

    test

---
+++ Tp1  Estructuras secuenciales Santiago Zabala.py
@@ -0,0 +1,90 @@
+#Alumno= Santiago Zabala
+
+#-------- Actividad 1
+
+#print ("Hola mundo!")
+
+#-------- Actividad 2
+
+#Nombre = input("Ingrese su nombre: ")
+#print(f"Hola " + Nombre + "!")
+
+
+#-------- Actividad 3
+
+#nombre = input("ingrese su nombre: ")
+#apellido = input("ingrese su apellido: ")
+#edad = input ("Ingrese su edad: ")
+#lugar_de_residencia = input ("Ingrese su lugar de residencia: ")
+
+#print(f"Soy " + nombre + ", "+ "tengo " + edad + " años San" + "y vivo en " + lugar_de_residencia)
+
+#-------- Actividad 4
+
+#Radio = input ("ingrese valor del radio: ")
+#Radio = int(Radio)
+#Pi = 3.14
+#Diametro =  Radio + Radio
+#Perimetro = Pi * Diametro
+#Area = Pi * Radio ** 2
+
+#print(f"El perímetro es {Perimetro} y el área es {Area}")
+
+#-------- Actividad 5
+
+#segundos = int(input("Ingresa la cantidad de segundos: "))
+#horas = segundos // 3600
+#print(f"Equivale a {horas} hora.")
+
+#-------- Actividad 6
+
+#numero = int(input("Ingrese un numero: "))
+
+#print(f"{numero} x 1 = {numero * 1}")
+#print(f"{numero} x 2 = {numero * 2}")
+#print(f"{numero} x 3 = {numero * 3}")
+#print(f"{numero} x 4 = {numero * 4}")
+#print(f"{numero} x 5 = {numero * 5}")
+#print(f"{numero} x 6 = {numero * 6}")
+#print(f"{numero} x 7 = {numero * 7}")
+#print(f"{numero} x 8 = {numero * 8}")
+#print(f"{numero} x 9 = {numero * 9}")
+#print(f"{numero} x 10 = {numero * 10}")
+
+#-------- Actividad 7
+
+#numero1 = int(input("Ingrese primer numero entero que no sea cero: "))
+#numero2 = int(input("Ingrese segundo numero entero que no sea cero: "))
+
+#print(f"{numero1+numero2}")
+#print(f"{numero1-numero2}")
+#print(f"{numero1*numero2}")
+#print(f"{numero1/numero2}")
+
+#-------- Actividad 8
+
+#Altura = float(input("Ingrese su altura en metros: "))
+#Peso = float(input("Ingrese su peso en Kilogramos: "))
+
+#IMC = Peso / (Altura ** 2)
+
+#print(f"{IMC}")
+
+#-------- Actividad 9
+
+#Celsius = float(input("Ingrese temperatura en celsius: "))
+
+#Fahrenheit = (9/5) * Celsius + 32
+
+#print(f"La temperatura en Fahrenheit es {Fahrenheit}")
+
+
+#-------- Actividad 10
+
+#N1 = float(input("Ingrese primer numero: "))
+#N2 = float(input("Ingrese segundo numero: "))
+#N3 = float(input("Ingrese tercer numero: "))
+
+#promedio = (N1 + N2 + N3) / 3
+
+#print(f"El promedio es {promedio}")
